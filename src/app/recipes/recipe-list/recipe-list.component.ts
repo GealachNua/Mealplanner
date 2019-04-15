fdf921ed5dd697be3b2a6d9fda97b65fd6437b2c
Christoph Wiekart <cwiekart@xs4all.nl>
2019-04-15 19:38:13 +0200

    Moved recipe item to its component. Added property binding from recipe-list to recipe-item.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,7 @@ export class RecipeListComponent implements OnInit {
         new Recipe('A Test Recipe', 'This is simpley a test', 'https://wereparents.com/wp-content/uploads/2012/09/IMG_2775.jpg'),
         new Recipe('Havermout pap', 'Havermout met melk. Daarnaast youghurt', 'https://blog.xxlnutrition.com/wp-content/uploads/2019/02/Havermout-Header-1024x536.jpg')
     ];
-
+    
   constructor() { }
 
   ngOnInit() {
