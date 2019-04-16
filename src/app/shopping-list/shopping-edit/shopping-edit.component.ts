762b5dece8e6ce9987970dbd0b74aabb73d0ad43
Christoph Wiekart <cwiekart@xs4all.nl>
2019-04-16 13:48:19 +0200

    New Ingredient Add functionality added.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+    @ViewChild('nameInput') nameInputRef: ElementRef;
+    @ViewChild('amountInput') amountInputRef: ElementRef;
+    @ViewChild('vetInput') vetInputRef: ElementRef;
+    @ViewChild('koolhInput') koolhInputRef: ElementRef;
+    @ViewChild('eiwitInput') eiwitInputRef: ElementRef;
+
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAdd() {
+      const ingName = this.nameInputRef.nativeElement.value;
+      const ingAmount = this.amountInputRef.nativeElement.value;
+      const ingVet = this.vetInputRef.nativeElement.value;
+      const ingKoolh = this.koolhInputRef.nativeElement.value;
+      const ingEiwit = this.eiwitInputRef.nativeElement.value;
+      const newIngredient = new Ingredient(ingName, ingAmount, ingVet, ingKoolh, ingEiwit);
+      this.ingredientAdded.emit(newIngredient);
+  }
 }
