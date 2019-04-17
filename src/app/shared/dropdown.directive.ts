befda77accc005461b2e299876d25e7509238972
Christoph Wiekart <cwiekart@xs4all.nl>
2019-04-17 12:29:42 +0200

    Made dropdown menu dynamic. Corrected the recipes component page so the pulldown menu button is not visible when no recipe is selected.

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,17 +1,23 @@
 import { Directive, HostListener, HostBinding, ElementRef, OnInit } from "@angular/core";
 
 
+
 @Directive({
     selector: '[appDropdown]'
 })
 
 export class DropdownDirective implements OnInit {
     constructor(private elementRef: ElementRef) {
+     }
 
-    }
+     @HostBinding('class') className: string = '';
+
+     @HostListener('click') mouseclick(eventData: Event){
+         this.className === 'open' ? this.className = '' : this.className = 'open';
+     }
 
     ngOnInit(){
-        this.elementRef.nativeElement.className = 'open';
+        // this.elementRef.nativeElement.className = 'open';
     }
 
 }
