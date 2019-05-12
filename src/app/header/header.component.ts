d77ac354477c0bba277618faffa4752aca4c37d7
Christoph Wiekart <cwiekart@xs4all.nl>
2019-05-12 21:35:06 +0200

    Replaced databinding with Angular Routing for navigation.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,29 +1,9 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-    @Output() menuBool = new EventEmitter<{recipe: boolean, list: boolean}>();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
- onRecipeSelected() {
-     this.menuBool.emit({
-         recipe: true,
-         list: false,
-     });
- }
-
-onListSelected() {
-    this.menuBool.emit({
-        recipe: false,
-        list: true,
-    });
-} 
-
+export class HeaderComponent {
 }
